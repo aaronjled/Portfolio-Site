a7386b709f1103906e670bc7d79d0c19969bf721
Aaron Ledbetter <aaronledbetter@Aarons-MacBook-Air.local>
2022-03-01 13:04:28 -1000

    got hamburger working, and completed slideshow

--- app.js
+++ app.js
@@ -38,3 +38,11 @@ const moveToPrevSlide = () => {
         slidePosition--;
     }
 }
+//hamburger
+const hamburger = document.querySelector(".hamburger");
+const topNav = document.querySelector("#nav-ul");
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    topNav.classList.toggle("active");
+})
+document.querySelectorAll(".nav-links").forEach(n => n.addEventListener)
