4991b8586aab2565340d8e4dddb21d912cfa785a
Aaron Ledbetter <aaronledbetter@Aarons-MacBook-Air.local>
2022-03-01 07:10:50 -1000

    slideshow is mostly set up

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
 console.log('Hello World')
+const slides = document.querySelectorAll('.slides');
+const slideNumber = 0
+slideshow();
+const slideshow = (slides) => {
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";  
+    }
+    slideNumber++;
+    if(slideNumber > slides.length) {
+        slideNumber = 1
+    }
+    slides[slideNumber-1].style.display = "block";
+    setTimeout(slideshow, 1500);
+    
+}
